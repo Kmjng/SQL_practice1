8321031b48ce8409e0d8ab7008db84ccf13e113b
Minjeong Kim <154487112+Kmjng@users.noreply.github.com>
2024-02-08 15:25:09 +0900

    Create select_exam03.sql

---
+++ chap02_SQL_select/select_exams/select_exam03.sql
@@ -0,0 +1,7 @@
+-- <연습문제3>
+
+--[문1] EMP 테이블에서 hiredate가 1981년 2월 20과 1981년 5월 1일 사이에 입사한 사원의 
+-- ename, job, hiredate을 출력하는 SELECT문 작성하기 (단 hiredate 순으로 오름차순 정렬) 
+
+
+--[문2] EMP 테이블에서 deptno가 10,20인 사원의 모든 정보를 출력하는 SELECT문 작성하기(단 ename으로 오름차순 정렬)
